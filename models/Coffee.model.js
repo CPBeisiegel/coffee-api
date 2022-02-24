70abcfbb7e96756587f46413d28f273d0ff3fe4a
Camila Beisiegel <camila.beisiegel@gmail.com>
2022-02-24 16:35:06 -0300

    adicionando readme

--- models/Coffee.model.js
+++ models/Coffee.model.js
@@ -1,37 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const coffeeSchema = new Schema({
-    name: {type: String, 
-           required: true,
-           unique: true, 
-           minLength: 1, 
-           trim: true
-        },
-    roast: {type: String,
-            enum: ["Blond Roast","Medium Roast","Dark Roast"], 
-            required: true
-        },
-    storageType: {
-        type: String, 
-        enum: ["Beans", "Grinded"],
-        required: true, 
-        default: "Beans"
-        },
-    // Como limitar a quantidade de elementos no array
-    sensoryNotes: [{type: String, minLength: 1, maxLength: 64}],
-    acidity: {type: Number, min: 1, max: 5, default: 3},
-    sweetness: {type: Number, min: 1, max: 5, default: 3},
-    bitterness: {type: Number, min: 1, max: 5, default: 3},
-
-
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 1,
+    trim: true,
+  },
+  roast: {
+    type: String,
+    enum: ["Blond Roast", "Medium Roast", "Dark Roast"],
+    required: true,
+  },
+  storageType: {
+    type: String,
+    enum: ["Beans", "Grinded"],
+    required: true,
+    default: "Beans",
+  },
+  // Como limitar a quantidade de elementos no array
+  sensoryNotes: [{ type: String, minLength: 1, maxLength: 64 }],
+  acidity: { type: Number, min: 1, max: 5, default: 3 },
+  sweetness: { type: Number, min: 1, max: 5, default: 3 },
+  bitterness: { type: Number, min: 1, max: 5, default: 3 },
 });
 
-
-
-
-
-
-
-module.exports = mongoose.model("Coffee", coffeeSchema )
+module.exports = mongoose.model("Coffee", coffeeSchema);
--- routes/coffee.routes.js
+++ routes/coffee.routes.js
@@ -1,82 +1,72 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 const CoffeeModel = require("../models/Coffee.model");
 
 router.post("/create-coffee", async (req, res) => {
-    try {
+  try {
+    const createCoffee = await CoffeeModel.create(req.body);
 
-        const createCoffee = await CoffeeModel.create(req.body);
-
-        return res.status(201).json(createCoffee);
-        
-    } catch (error) {
-        console.error(error)
-        return res.status(500).json(error)
-    }
-})
+    return res.status(201).json(createCoffee);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+});
 
 router.get("/all-coffees", async (req, res) => {
-    try {
+  try {
+    const allCoffees = await CoffeeModel.find({});
 
-        const allCoffees = await CoffeeModel.find({})
-
-        return res.status(201).json(allCoffees);
-        
-    } catch (error) {
-        console.error(error)
-        return res.status(500).json(error)
-    }
-})
+    return res.status(201).json(allCoffees);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+});
 
 router.get("/coffee/:id", async (req, res) => {
-   
-    try {
-        // Pensar neste espaço como algo livre para criar a nossa lógica de acordo com a nossa necessidade
-        const { id } = req.params
-        const coffee = await CoffeeModel.findOne({_id: id })
+  try {
+    // Pensar neste espaço como algo livre para criar a nossa lógica de acordo com a nossa necessidade
+    const { id } = req.params;
+    const coffee = await CoffeeModel.findOne({ _id: id });
 
-        return res.status(200).json(coffee);
-        
-    } catch (error) {
-        console.error(error)
-        return res.status(500).json(error)
-    }
-})
+    return res.status(200).json(coffee);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+});
 
 router.patch("/edit-coffee/:id", async (req, res) => {
-    try {
-        const { id } = req.params
+  try {
+    const { id } = req.params;
 
-        const updatedCoffee = await CoffeeModel.findOneAndUpdate(
-            {_id: id }, 
-            {...req.body},
-            // colocamos esse para retornar o valor atualizado pois sem ele só atualiza no banco de dados
-            {new: true} 
-            )
+    const updatedCoffee = await CoffeeModel.findOneAndUpdate(
+      { _id: id },
+      { ...req.body },
+      // colocamos esse para retornar o valor atualizado pois sem ele só atualiza no banco de dados
+      { new: true }
+    );
 
-            return res.status(200).json(updatedCoffee)
-
-        
-    } catch (error) {
-        console.error(error)
-        return res.status(500).json(error)
-    }
-})
+    return res.status(200).json(updatedCoffee);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+});
 
 router.delete("/delete-coffee/:id", async (req, res) => {
-    try {
-        const { id } = req.params
-        const deleteCoffee = await CoffeeModel.deleteOne({_id: id})
+  try {
+    const { id } = req.params;
+    const deleteCoffee = await CoffeeModel.deleteOne({ _id: id });
 
-        return res.status(200).json(deleteCoffee)
-        
-    } catch (error) {
-        console.error(error)
-        return res.status(500).json(error)
-    }
-})
-
+    return res.status(200).json(deleteCoffee);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+});
 
 module.exports = router;
