291d7c0e8616926cd2a8028eb37ce12fa8cd4f90
Camila Beisiegel <camila.beisiegel@gmail.com>
2022-02-24 12:15:39 -0300

    criando uma api de coffee

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+// Isso nos dá acesso aos documentos dentro do env
+require("dotenv").config();
+// Conectando a config do banco de dados 
+require("./config/db.config")()
+
+const app = express();
+
+app.use(express.json());
+
+
+const coffeeRouter = require("./routes/coffee.routes");
+app.use("/coffee-inventory", coffeeRouter);
+
+
+
+
+
+
+
+
+// Colocar o express para ouvir as requisições
+app.listen(Number(process.env.PORT), () => {
+    console.log(`Server up and running #d&d - port: ${process.env.PORT}`);
+})
